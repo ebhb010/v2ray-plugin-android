d673bcd58927e152e4713222bbccc678b5fb4026
ebhb010 <ebhb010@gmx.com>
2024-10-03 18:58:14 +0000

    fix go script

--- app/go-build.bash
+++ app/go-build.bash
@@ -28,7 +28,7 @@ for i in "${!ABIS[@]}"; do
         CGO_ENABLED=1 CC="${TOOLCHAIN}/${CLANG_ARCHS[$i]}${MIN_API}-clang" \
         GOOS=android GOARCH=${GO_ARCHS[$i]} \
         go build -v -ldflags='-s -w' -gcflags=all='-l -B -C' -o "${OUT_DIR}/unstripped" \
-    && "${TOOLCHAIN}/${STRIP_ARCHS[$i]}-strip" "${OUT_DIR}/unstripped" -o "${OUT_DIR}/${ABI}/${BIN}" \
+    && "${TOOLCHAIN}/llvm-strip" --strip-unneeded "${OUT_DIR}/unstripped" -o "${OUT_DIR}/${ABI}/${BIN}" \
     || exit -1
     rm "${OUT_DIR}/unstripped"
 done
